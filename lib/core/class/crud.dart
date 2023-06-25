d3324e51dc7554eef170d630201254e222a65bbc
Ahmed Hakimi <ajozalhakimey101@gmail.com>
2023-06-25 23:44:41 +0300

    added class crud and status request

---
+++ lib/core/class/crud.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce/core/class/statusRequest.dart';
+import 'package:e_commerce/core/functions/checkConnection.dart';
+import 'package:http/http.dart' as http;
+
+class Crud {
+  Future<Either<StatusRequest, Map>> postData(String url, Map data) async {
+    if (await checkInternet()) {
+      var response = await http.post(Uri.parse(url), body: data);
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        Map responseBody = json.decode(response.body);
+        return Right(responseBody);
+      }
+    } else {
+      return const Left(StatusRequest.offlineFailure);
+    }
+    throw Exception();
+  }
+}
