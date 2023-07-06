03786d05b1771735d5e9187c2cc2f8a3841aef77
Ahmed Hakimi <ajozalhakimey101@gmail.com>
2023-07-06 23:04:30 +0300

    better division of widgets and screen [CART]

--- lib/view/screen/cart/cart_screen.dart
+++ lib/view/screen/cart/cart_screen.dart
@@ -1,204 +1,43 @@
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:e_commerce/core/constant/colors.dart';
-import 'package:e_commerce/core/constant/image_asset.dart';
-import 'package:e_commerce/view/widget/customappButton.dart';
+import 'package:e_commerce/data/model/items.dart';
+import 'package:e_commerce/view/widget/cart/card_cart.dart';
+import 'package:e_commerce/view/widget/cart/cart_quantity_card.dart';
+import 'package:e_commerce/view/widget/cart/appbarCart.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttericon/iconic_icons.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
-
-import '../../widget/cart/orderbutton.dart';
+import '../../widget/cart/bottomappCart.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
-
+  // final ItemsModel? itemsModel;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                  margin: EdgeInsets.symmetric(horizontal: Get.width / 15),
-                  child: Text("Total : ",
-                      style: TextStyle(fontSize: Get.width / 16))),
-              Container(
-                width: Get.width / 3,
-                decoration: BoxDecoration(
-                    color: AppColor.primary,
-                    borderRadius: BorderRadius.circular(20)),
-                margin: EdgeInsets.symmetric(horizontal: Get.width / 15),
-                child: Text(
-                  textAlign: TextAlign.center,
-                  "600 \$",
-                  style: TextStyle(
-                      fontSize: Get.width / 16,
-                      color: AppColor.white,
-                      fontFamily: "sans"),
-                ),
-              ),
-            ],
-          ),
-          const CutomOrderButton(),
-        ],
-      ),
+      bottomNavigationBar: const BottomAppCart(),
       body: ListView(
         children: [
-          Row(
-            children: [
-              IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
-              Container(
-                alignment: Alignment.center,
-                child: Text(
-                  "My Cart",
-                  style: TextStyle(fontSize: Get.width / 10),
-                ),
-              ),
-            ],
-          ),
+          CartAppBar(title: "My Cart", onPressed: () {}),
           SizedBox(
             height: Get.width / 19,
           ),
+          CartQuantity(count: "3"),
           Container(
-            margin: EdgeInsets.symmetric(horizontal: 15),
-            height: Get.width / 10,
-            padding: const EdgeInsets.only(top: 5),
-            decoration: BoxDecoration(
-                color: AppColor.primary,
-                borderRadius: BorderRadius.circular(20)),
-            child: Text("You have 2 Items in the cart",
-                textAlign: TextAlign.center,
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyText1!
-                    .copyWith(color: AppColor.white)),
-          ),
-          Container(
-            padding: EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
             child: Column(
               children: [
-                Card(
-                  elevation: 10,
-                  child: Container(
-                    child: Row(
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: CachedNetworkImage(
-                              imageUrl: ImageAsset.logo,
-                              fit: BoxFit.fitHeight,
-                              height: 90),
-                        ),
-                        Expanded(
-                          flex: 3,
-                          child: ListTile(
-                            title: Text(
-                              "Mac  M2 max",
-                              style: Theme.of(context).textTheme.bodyText1,
-                            ),
-                            subtitle: Text(
-                              "\$300",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyLarge!
-                                  .copyWith(color: AppColor.primary),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            children: [
-                              Container(
-                                height: 36,
-                                child: IconButton(
-                                  onPressed: () {},
-                                  icon: const Icon(Iconic.plus,
-                                      color: AppColor.forGradient),
-                                ),
-                              ),
-                              Text(
-                                "2",
-                                style:
-                                    TextStyle(fontFamily: "sans", fontSize: 20),
-                              ),
-                              SizedBox(
-                                height: 35,
-                                child: IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(
-                                      Iconic.minus,
-                                      color: AppColor.forGradient,
-                                    )),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                CustomCartCard(
+                  name: "Mac",
+                  price: "300",
+                  quantity: "1",
                 ),
-                Card(
-                  elevation: 10,
-                  child: Container(
-                    child: Row(
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: CachedNetworkImage(
-                              imageUrl: ImageAsset.onBoardingImageThree,
-                              fit: BoxFit.fitHeight,
-                              height: 90),
-                        ),
-                        Expanded(
-                          flex: 3,
-                          child: ListTile(
-                            title: Text(
-                              "Mac  M2 max",
-                              style: Theme.of(context).textTheme.bodyText1,
-                            ),
-                            subtitle: Text(
-                              "\$300",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyLarge!
-                                  .copyWith(color: AppColor.primary),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            children: [
-                              Container(
-                                height: 36,
-                                child: IconButton(
-                                  onPressed: () {},
-                                  icon: const Icon(Iconic.plus,
-                                      color: AppColor.forGradient),
-                                ),
-                              ),
-                              Text(
-                                "2",
-                                style:
-                                    TextStyle(fontFamily: "sans", fontSize: 20),
-                              ),
-                              SizedBox(
-                                height: 35,
-                                child: IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(
-                                      Iconic.minus,
-                                      color: AppColor.forGradient,
-                                    )),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                CustomCartCard(
+                  name: "Airpods",
+                  price: "90",
+                  quantity: "2",
+                ),
+                CustomCartCard(
+                  name: "Xaomi Redmi Note 10",
+                  price: "200",
+                  quantity: "1",
                 ),
               ],
             ),
