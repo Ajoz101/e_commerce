9b420159637ff62b978d844464976c114afb785a
Ahmed Hakimi <ajozalhakimey101@gmail.com>
2023-07-06 22:04:17 +0300

    design of cart Screen part 4

--- lib/view/screen/cart/cart_screen.dart
+++ lib/view/screen/cart/cart_screen.dart
@@ -1,9 +1,13 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:e_commerce/core/constant/colors.dart';
 import 'package:e_commerce/core/constant/image_asset.dart';
+import 'package:e_commerce/view/widget/customappButton.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttericon/iconic_icons.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
+
+import '../../widget/cart/orderbutton.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -11,6 +15,19 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text("Total : ", style: TextStyle(fontSize: Get.width / 14)),
+              Text("600 \$", style: TextStyle(fontSize: Get.width / 14)),
+            ],
+          ),
+          const CutomOrderButton(),
+        ],
+      ),
       body: ListView(
         children: [
           Container(
@@ -25,59 +42,16 @@ class CartScreen extends StatelessWidget {
             child: Column(
               children: [
                 Card(
-                  elevation: 14,
-                  child: Container(
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: CachedNetworkImage(imageUrl: ImageAsset.logo),
-                        ),
-                        Expanded(
-                          flex: 3,
-                          child: ListTile(
-                            title: Text(
-                              "Dell  latitude",
-                              style: Theme.of(context).textTheme.bodyText1,
-                            ),
-                            subtitle: Text(
-                              "\$300",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyLarge!
-                                  .copyWith(color: AppColor.primary),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Column(children: [
-                            IconButton(
-                              onPressed: () {},
-                              icon: Icon(Icons.add),
-                            ),
-                            Text(
-                              "2",
-                              style:
-                                  TextStyle(fontFamily: "sans", fontSize: 25),
-                            ),
-                            IconButton(
-                              onPressed: () {},
-                              icon: Icon(Icons.minimize),
-                            ),
-                          ]),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Card(
-                  elevation: 14,
+                  elevation: 10,
                   child: Container(
                     child: Row(
                       children: [
                         Expanded(
                           flex: 2,
                           child: CachedNetworkImage(
-                              imageUrl: ImageAsset.onBoardingImageOne),
+                              imageUrl: ImageAsset.logo,
+                              fit: BoxFit.fitHeight,
+                              height: 90),
                         ),
                         Expanded(
                           flex: 3,
@@ -102,8 +76,8 @@ class CartScreen extends StatelessWidget {
                                 height: 36,
                                 child: IconButton(
                                   onPressed: () {},
-                                  icon: Icon(FontAwesomeIcons.plus,
-                                      color: AppColor.primary),
+                                  icon: const Icon(Iconic.plus,
+                                      color: AppColor.forGradient),
                                 ),
                               ),
                               Text(
@@ -111,12 +85,72 @@ class CartScreen extends StatelessWidget {
                                 style:
                                     TextStyle(fontFamily: "sans", fontSize: 20),
                               ),
-                              Container(
-                                height: 30,
+                              SizedBox(
+                                height: 35,
                                 child: IconButton(
                                     onPressed: () {},
-                                    icon: Icon(
-                                      FontAwesomeIcons.minus,
+                                    icon: const Icon(
+                                      Iconic.minus,
+                                      color: AppColor.forGradient,
+                                    )),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 10,
+                  child: Container(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: CachedNetworkImage(
+                              imageUrl: ImageAsset.onBoardingImageThree,
+                              fit: BoxFit.fitHeight,
+                              height: 90),
+                        ),
+                        Expanded(
+                          flex: 3,
+                          child: ListTile(
+                            title: Text(
+                              "Mac  M2 max",
+                              style: Theme.of(context).textTheme.bodyText1,
+                            ),
+                            subtitle: Text(
+                              "\$300",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyLarge!
+                                  .copyWith(color: AppColor.primary),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 36,
+                                child: IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(Iconic.plus,
+                                      color: AppColor.forGradient),
+                                ),
+                              ),
+                              Text(
+                                "2",
+                                style:
+                                    TextStyle(fontFamily: "sans", fontSize: 20),
+                              ),
+                              SizedBox(
+                                height: 35,
+                                child: IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Iconic.minus,
                                       color: AppColor.forGradient,
                                     )),
                               ),
---
+++ lib/view/widget/cart/orderbutton.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:fluttericon/iconic_icons.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+
+import '../../../core/constant/colors.dart';
+
+class CutomOrderButton extends StatelessWidget {
+  const CutomOrderButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      child: ActionChip(
+        elevation: 20,
+        backgroundColor: AppColor.cards,
+        label: const Text("Order",
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: AppColor.primary)),
+        onPressed: () {},
+        avatar: Icon(Iconic.ok_circle,
+            color: AppColor.primary, size: Get.width / 14),
+      ),
+    );
+  }
+}
